10a48cd4f84338a6f09c1e408238b2c4ea965e7d
Vivek <vs2575@columbia.edu>
2017-02-04 01:00:28 -0500

    initial commit

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from credentials import ACCOUNT_SID, AUTH_TOKEN, MY_CELL, MY_TWILIO
+from twilio.rest import TwilioRestClient
+
+import twilio.twiml 
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+ 
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-@app.route("/request")
-def request():
-	return 0;
+@app.route("/send_sms")
+def send_sms(_body, _media_url):
+	client.messages.create(
+    to=MY_CELL, 
+    from_=MY_TWILIO, 
+    body=_body, 
+    media_url=_media_url, 
+)
 
-@app.route("/response")
-def response():
-	return 0;
+@app.route("/", methods=['GET','POST'])
+def response(_body, _media_url):
+	resp = twilio.twiml.Response()
+
+	if (_media_url == None):
+		resp.message(_body)
+	else:
+		with resp.message(_body) as m:
+			m.media(_media_url)
+		
+	return str(resp)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
