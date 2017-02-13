b7fb32b778867bd02ee3e25bc5ed324abb78ac1b
Vivek Subramaniam <vsub21@users.noreply.github.com>
2017-02-13 10:31:43 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # put your own credentials here 
-ACCOUNT_SID = "AC69ae1a62ef1ee1a8c681b60c56776c4f" 
-AUTH_TOKEN = "0c2108fb8a53cddc90adeb84238ecbef" 
-MY_CELL = "+15084393101"
-MY_TWILIO = "+17744625236"
+ACCOUNT_SID = "" 
+AUTH_TOKEN = "" 
+MY_CELL = ""
+MY_TWILIO = ""
